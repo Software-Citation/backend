f664afeb49e21780784a1b422aa61125e1d585a3
Chas Nelson <cvs@chasnelson.co.uk>
2020-09-03 11:58:53 +0100

    push initial cookie cutter

---
+++ software_citation/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from software_citation.users.models import User
+from software_citation.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
